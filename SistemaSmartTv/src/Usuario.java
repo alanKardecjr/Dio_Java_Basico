87eaac68acbfe1948f2a42e4b8fad55eab9bcf1b
alan kardec <akardec35@gmail.com>
2024-08-27 12:59:39 -0300

    Atualização do App

--- SistemaSmartTv/src/Usuario.java
+++ SistemaSmartTv/src/Usuario.java
@@ -3,9 +3,28 @@ public class Usuario {
        
         SmartTv smartTv = new SmartTv();
 
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Canal Atual: " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
         System.out.println("Tv Ligada ?" + smartTv.ligada);
         System.out.println("Canal Atual: " + smartTv.canal);
         System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.ligar ();
+        System.out.println("Novo Status -> TV Ligada ?" + smartTv.ligada);
+
+        smartTv.desligada ();
+        System.out.println("Novo Status -> TV Ligada ?" + smartTv.ligada);
+
 
     }
 }
